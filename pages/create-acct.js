76f429195e10f6a2132d4e9c621035c9cf1f06dc
Diego Taveras <diegotaveras2@outlook.com>
2020-05-02 16:01:33 -0400

    css for Forms

--- pages/create-acct.js
+++ pages/create-acct.js
@@ -1,14 +1,51 @@
 import React from 'react'
 import styled from '@emotion/styled';
-
+import {css} from '@emotion/core'
 import Layout from '../components/layouts/Layout';
-
+import { Form, Field, InputSubmt } from './../components/ui/Form';
 
 export default function CreateAcct() {
   return (
     <div> 
       <Layout>
-          <h1>Create Account</h1>
+        <>
+          <h1 css={css`text-align: center;margin-top: 5rem;`}
+            >Create Account
+          </h1>
+          <Form action="">
+            <Field>
+              <label htmlFor="name">Name</label>
+              <input 
+                type="text" 
+                name="name" 
+                id="name"
+                placeholder="Your name"
+              />
+            </Field>
+            <Field>
+              <label htmlFor="email">Email</label>
+              <input 
+                type="email" 
+                name="email" 
+                id="email"
+                placeholder="email"
+              />
+            </Field>
+            <Field>
+              <label htmlFor="password">Password</label>
+              <input 
+                type="password" 
+                name="password" 
+                id="password"
+                placeholder="Your password"
+              />
+            </Field>
+            <InputSubmt 
+              type="submit"
+              value="Create account"
+            />
+          </Form>
+        </>
       </Layout>
     </div>
   )
