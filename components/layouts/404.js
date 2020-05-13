a2d4cc9207b5e5e8b9784d31faea83ae3ed1da91
Diego Taveras <diegotaveras2@outlook.com>
2020-05-13 01:06:14 -0400

    validating that product exist

---
+++ components/layouts/404.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {css} from '@emotion/core'
+
+const Error404 = () => {
+    return (  <h1 css={css`margin-top: 5rem; text-align: center;`}>Product dosnt exist</h1>);
+}
+ 
+export default Error404;
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React,{useEffect,useContext,useState} from 'react'
+import { useRouter } from 'next/router';
+import FirebaseContext from './../../firebase/context';
+import Error404 from './../../components/layouts/404';
+import Layout from './../../components/layouts/Layout';
 
 const Product = () => {
-    return (<h1>Desde [id].js</h1>  );
+
+    const [product,setProduct] = useState({})
+    const [error,setError] = useState(false);
+
+    // Routing to get actual id
+    const router = useRouter();
+    const {query: {id}} = router;
+
+    // firebase context
+    const {firebase} = useContext(FirebaseContext)
+
+    useEffect( ()=>{
+        if(id){
+            const getProduct = async () =>{
+                const productQuery = await firebase.db.collection('products').doc(id);
+                const product = await productQuery.get();
+                if(product.exist){
+                    setProduct(product.data())
+                }else{
+                    setError(true)
+                }
+            }
+            getProduct()
+        }
+    },[id])
+  
+    return (
+    <Layout>
+        {error && <Error404/>}
+    </Layout>  );
 }
  
 export default Product;
